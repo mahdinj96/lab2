030f5e3f0af89713cc00d0b48761b6f0fcfa6eb1
Mahdi <mahdi.naimi@gmail.com>
2021-03-04 18:26:54 -0700

    New map using Leaflet

--- application.py
+++ application.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 
 from flask import Flask, session, render_template, request, make_response, redirect, url_for
@@ -23,30 +22,4 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-=======
-import os
-
-from flask import Flask, session, render_template, request, make_response, redirect, url_for
-from flask_session import Session
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-
-app = Flask(__name__)
-
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
-
-# Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
-# Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
-
-@app.route("/")
-def index():
->>>>>>> 0f49385920fd1551a7f5b6bc6aff354cd0eb1b47
     return render_template("index.html")
